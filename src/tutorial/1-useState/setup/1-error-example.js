c183e19a6da0856b543bb5827556e96900e1720c
Oleg Tielushko <tielushko@mail.usf.edu>
2020-10-15 22:00:48 -0400

    intro to hooks, useState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "random title";
+
+  //while we are changing the value of the variable, we are not invoking the rerendering of the application. we need to find a way to preserve the previous value and rerender application to switch between two states.
+  const handleClick = () => {
+    title = "hello people";
+    console.log(title);
+  };
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default ErrorExample;
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+
+//use state - function from the React js. also it is a named import, must must must be exact.
+
+//any use... function in react requires the component name to be uppercase
+//must be in function/component body
+//cannot call conditionally.
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  //setting up the values from useState - text then by convention setVariableName
+  const [text, setText] = useState("random title");
+
+  //essentially use state allows us to use React to Rerender the webpage components.
+
+  const handleClick = () => {
+    //use the setText to change the text whatever we pass will be the new value
+    if (text === "random title") {
+      setText("Hello another title");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" type="button" onClick={handleClick}>
+        Change the Header with useState
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
